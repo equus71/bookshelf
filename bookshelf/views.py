d0a625585b635ef4c5ad535b7058d306d9322813
equus71 <grzegorz.biziel@gmail.com>
2015-11-23 23:03:15 +0100

    Add django app serving the data

--- bookshelf/views.py
+++ bookshelf/views.py
@@ -1,4 +1,5 @@
-from django.http import JsonResponse
+import json
+from django.http import JsonResponse, HttpResponse
 from django.views.generic import TemplateView
 from bookshelf import settings
 
@@ -13,27 +14,44 @@ class IndexView(TemplateView):
 
 
 def books(request):
+    with open('bookshelf/books.json') as data_file:
+        data = json.load(data_file)
 
-    # do something with the your data
-    data = {}
-
-    # just return a JsonResponse
-    return JsonResponse(data)
+        return JsonResponse(data, safe=False)
 
 
-def book_by_id(request):
+def book_by_id(request, book_id):
+    with open('bookshelf/books.json') as data_file:
+        data = json.load(data_file)
 
-    # do something with the your data
-    data = {}
+        try:
+            book = get_book_by_id(data, book_id)
+        except StopIteration:
+            return JsonResponse({}, status=404)
 
-    # just return a JsonResponse
-    return JsonResponse(data)
+        return JsonResponse(book, safe=False)
 
 
-def recommended_for_book_by_id(request):
+def recommended_for_book_by_id(request, book_id):
+    with open('bookshelf/books.json') as data_file:
+        data = json.load(data_file)
 
-    # do something with the your data
-    data = {}
+        try:
+            book = get_book_by_id(data, book_id)
+        except StopIteration:
+            return JsonResponse({}, status=404)
 
-    # just return a JsonResponse
-    return JsonResponse(data)
+        recommendations = get_recommendation_for_book(data, book)
+
+        return JsonResponse(recommendations, safe=False)
+
+
+def get_book_by_id(books_data, book_id):
+    return next(book for book in books_data if book.get('id') == book_id)
+
+
+def get_recommendation_for_book(books_data, source_book):
+    return [book for book in books_data if
+            book.get('genre', {}).get('name') == source_book.get('genre', {}).get('name')
+            and book.get('genre', {}).get('category') == source_book.get('genre', {}).get('category')
+            and book.get('id') != source_book.get('id')]
